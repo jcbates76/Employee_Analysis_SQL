2361e8c2a759c284e6d9d4188b17be5918c9fa2b
Jason Bates <94392882+jcbates76@users.noreply.github.com>
2022-03-05 07:22:48 -0600

    Completed SQL

---
+++ EmployeeSQL/queries.sql
@@ -0,0 +1,91 @@
+-- Queries on the data tables created
+
+-- 1) List the following details of each employuee:
+-- employee number, last name, first name, sex, and salary
+CREATE VIEW employee_info AS
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+INNER JOIN salaries ON
+employees.emp_no = salaries.emp_no;
+
+SELECT * FROM employee_info;
+
+-- 2) List first name, last name, and hire date for employees who were hired in 1986.
+CREATE VIEW employee_hire_post_1986 AS
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date >= '1986-01-01' and hire_date <= '1986-12-31';
+
+SELECT * FROM employee_hire_post_1986;
+
+-- 3) List the manager of each department with the following information:
+-- department number, department name, the manager's employee number, last name, first name
+CREATE VIEW department_managers AS
+SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM dept_manager
+INNER JOIN departments
+ON dept_manager.dept_no = departments.dept_no
+INNER JOIN employees
+ON dept_manager.emp_no = employees.emp_no;
+
+SELECT * FROM department_managers;
+
+Select * from employees;
+
+-- 4) List the department of each employee with the following information:
+-- employee number, last name, first name, and department name
+CREATE VIEW department_employees AS
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+INNER JOIN dept_emp
+ON employees.emp_no = dept_emp.emp_no
+INNER JOIN departments
+ON dept_emp.dept_no = departments.dept_no;
+
+SELECT * FROM department_employees;
+
+-- 5) List first name, last name, and sex for employees
+-- whose first name is "Hercules" and last names begin with "B".
+CREATE VIEW hercules_b AS
+SELECT first_name, last_name, sex FROM employees
+WHERE first_name = 'Hercules' 
+AND last_name LIKE 'B%';
+
+SELECT * from hercules_b;
+
+-- 6) List all employees in the Sales department,
+-- including their employee number, last name, first name, and department name.
+CREATE VIEW sales_emp AS
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name 
+FROM employees
+INNER JOIN dept_emp
+ON employees.emp_no = dept_emp.emp_no
+INNER JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE dept_name = 'Sales';
+
+SELECT * FROM sales_emp;
+
+-- 7) List all employees in the Sales and Development departments, 
+-- including their employee number, last name, first name, and department name.
+CREATE VIEW sales_development_emp AS
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name 
+FROM employees
+INNER JOIN dept_emp
+ON employees.emp_no = dept_emp.emp_no
+INNER JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE dept_name = 'Sales' or dept_name = 'Development';
+
+SELECT * FROM sales_development_emp;
+
+-- 8) In descending order, list the frequency count of employee last names, 
+-- i.e., how many employees share each last name.
+
+CREATE VIEW last_name_count AS
+SELECT last_name, count(emp_no) AS "count_last_name"
+FROM employees
+GROUP BY last_name
+ORDER BY "count_last_name" DESC;
+
+select * from last_name_count;
---
+++ EmployeeSQL/table_schema_v02.sql
@@ -0,0 +1,81 @@
+﻿-- TABLE SCHEMA FOR sql_challenge
+
+-- Create departments table
+DROP TABLE IF EXISTS departments;
+
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(4) NOT NULL PRIMARY KEY,
+    "dept_name" VARCHAR(30)   NOT NULL
+);
+
+-- Verify departments data
+SELECT * FROM departments;
+
+-- Create employees table
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE "employees" (
+    "emp_no" int NOT NULL PRIMARY KEY,
+    "emp_title_id" VARCHAR(5) NOT NULL,
+    "birth_date" DATE NOT NULL,
+    "first_name" VARCHAR(30) NOT NULL,
+    "last_name" VARCHAR(30) NOT NULL,
+    "sex" VARCHAR(1) NOT NULL,
+    "hire_date" DATE NOT NULL
+);
+
+-- Verify employees data
+SELECT * FROM employees;
+
+-- Create salaries table
+DROP TABLE IF EXISTS salaries;
+
+CREATE TABLE "salaries" (
+    "emp_no" INT NOT NULL UNIQUE,
+    "salary" int NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+-- Verify salaries data
+select * from salaries;
+
+-- Create titles table
+DROP TABLE IF EXISTS titles;
+
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(5) NOT NULL PRIMARY KEY,
+    "title" VARCHAR(30) NOT NULL
+);
+
+-- Verify titles data
+SELECT * FROM titles;
+
+-- Create department/employee table
+DROP TABLE IF EXISTS dept_emp;
+
+CREATE TABLE "dept_emp" (
+    "emp_no" INT NOT NULL,
+    "dept_no" VARCHAR(4) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	CONSTRAINT dept_emp_key PRIMARY KEY (emp_no, dept_no)
+);
+
+-- Verify department/employee data
+SELECT * FROM dept_emp;
+
+-- Create dept/manager table
+DROP TABLE IF EXISTS dept_manager;
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR(4) NOT NULL,
+    "emp_no" INT NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	CONSTRAINT dept_emp_mgr_key PRIMARY KEY (dept_no, emp_no)
+);
+
+-- Verify dept/manager data
+SELECT * FROM dept_manager;
+
+-- All tables have been created.  
